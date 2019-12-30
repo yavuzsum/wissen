3b19d9b9920dc8c17c475fd24f30eec020e890b6
Yavuz Selim Sümbül <40361490+yavuzsum@users.noreply.github.com>
2019-12-30 19:24:46 +0300

    FirstFolders
    deneme

---
+++ AdoGiris/AdoGiris/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace AdoGiris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUnvan = new System.Windows.Forms.ComboBox();
+            this.txtSehir = new System.Windows.Forms.ComboBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnKayit = new System.Windows.Forms.Button();
+            this.txtSehirId = new System.Windows.Forms.TextBox();
+            this.Liste = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Unvan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dadi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sehir = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.txtIlceler = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Liste)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUnvan
+            // 
+            this.txtUnvan.FormattingEnabled = true;
+            this.txtUnvan.Items.AddRange(new object[] {
+            "Prof.Dr.",
+            "Doc.Dr.",
+            "Uzman Dr.",
+            "Dr."});
+            this.txtUnvan.Location = new System.Drawing.Point(187, 78);
+            this.txtUnvan.Name = "txtUnvan";
+            this.txtUnvan.Size = new System.Drawing.Size(294, 21);
+            this.txtUnvan.TabIndex = 0;
+            // 
+            // txtSehir
+            // 
+            this.txtSehir.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.txtSehir.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.txtSehir.FormattingEnabled = true;
+            this.txtSehir.Location = new System.Drawing.Point(187, 154);
+            this.txtSehir.Name = "txtSehir";
+            this.txtSehir.Size = new System.Drawing.Size(294, 21);
+            this.txtSehir.TabIndex = 0;
+            this.txtSehir.SelectedIndexChanged += new System.EventHandler(this.txtSehir_SelectedIndexChanged);
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(187, 117);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(294, 20);
+            this.txtAd.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(81, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ünvan :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(81, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Doktor Adı :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(81, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Şehir :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnKayit
+            // 
+            this.btnKayit.Location = new System.Drawing.Point(631, 83);
+            this.btnKayit.Name = "btnKayit";
+            this.btnKayit.Size = new System.Drawing.Size(75, 23);
+            this.btnKayit.TabIndex = 3;
+            this.btnKayit.Text = "Kaydet";
+            this.btnKayit.UseVisualStyleBackColor = true;
+            this.btnKayit.Click += new System.EventHandler(this.btnKayit_Click);
+            // 
+            // txtSehirId
+            // 
+            this.txtSehirId.Location = new System.Drawing.Point(487, 154);
+            this.txtSehirId.Name = "txtSehirId";
+            this.txtSehirId.Size = new System.Drawing.Size(100, 20);
+            this.txtSehirId.TabIndex = 4;
+
+            // 
+            // Liste
+            // 
+            this.Liste.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Liste.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Unvan,
+            this.Dadi,
+            this.Sehir});
+            this.Liste.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Liste.Location = new System.Drawing.Point(0, 300);
+            this.Liste.Name = "Liste";
+            this.Liste.Size = new System.Drawing.Size(800, 150);
+            this.Liste.TabIndex = 5;
+            this.Liste.DoubleClick += new System.EventHandler(this.Liste_DoubleClick);
+            // 
+            // Id
+            // 
+            this.Id.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.Width = 41;
+            // 
+            // Unvan
+            // 
+            this.Unvan.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Unvan.HeaderText = "Ünvan";
+            this.Unvan.Name = "Unvan";
+            this.Unvan.Width = 64;
+            // 
+            // Dadi
+            // 
+            this.Dadi.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Dadi.HeaderText = "Doktor Adı";
+            this.Dadi.Name = "Dadi";
+            // 
+            // Sehir
+            // 
+            this.Sehir.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Sehir.HeaderText = "Şehir";
+            this.Sehir.Name = "Sehir";
+            this.Sehir.Width = 56;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(631, 117);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncelle.TabIndex = 6;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(631, 154);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 6;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // txtIlceler
+            // 
+            this.txtIlceler.FormattingEnabled = true;
+            this.txtIlceler.Location = new System.Drawing.Point(187, 194);
+            this.txtIlceler.Name = "txtIlceler";
+            this.txtIlceler.Size = new System.Drawing.Size(294, 21);
+            this.txtIlceler.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Yellow;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(81, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "İlçeler:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtIlceler);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.Liste);
+            this.Controls.Add(this.txtSehirId);
+            this.Controls.Add(this.btnKayit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtSehir);
+            this.Controls.Add(this.txtUnvan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Liste)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox txtUnvan;
+        private System.Windows.Forms.ComboBox txtSehir;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnKayit;
+        private System.Windows.Forms.TextBox txtSehirId;
+        private System.Windows.Forms.DataGridView Liste;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Unvan;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dadi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sehir;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.ComboBox txtIlceler;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AdoGiris/AdoGiris/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace AdoGiris
+{
+    /*
+    ADO.NET=Active Data Object
+    Veritabanı ile C# dilini konuşturmak için kullanılan teknolojilerden birisidir.
+    1.Connected(Bağlantılı model)
+    2.Disconnected(Bağlantısız model) 
+
+        DataSource<-->DataProvider-->DataSet
+        -->Asp.Net | -->WindowsForm | -->WebService | -->Security etc.
+    */
+    public partial class Form1 : Form
+    {
+        SqlCommand uygula;
+        SqlDataReader oku;
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["CON"].ToString());
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ComboDoldur();
+            Listele();
+        }
+
+        private void Listele()
+        {
+            Liste.Rows.Clear();
+            int i = 0;
+            conn.Open();
+            uygula = new SqlCommand("Select * from tblDoktors",conn);
+            SqlDataReader oku = null;
+            oku = uygula.ExecuteReader();
+
+            foreach(var k in oku)
+            {
+                Liste.Rows.Add();
+                Liste.Rows[i].Cells[0].Value = oku["Id"].ToString();
+                Liste.Rows[i].Cells[1].Value = oku["Unvan"].ToString();
+                Liste.Rows[i].Cells[2].Value = oku["DoktorAdi"].ToString();
+                Liste.Rows[i].Cells[3].Value = oku["SehirId"].ToString();
+                i++;
+            }
+            Liste.AllowUserToAddRows = false;
+            oku.Close();
+            conn.Close();
+        }
+
+        private void ComboDoldur()
+        {
+            txtSehir.AutoCompleteSource=AutoCompleteSource.CustomSource;
+            AutoCompleteStringCollection veri = new AutoCompleteStringCollection();
+            string query = "Select * from iller";
+            uygula = new SqlCommand(query, conn);
+            uygula.Connection.Open();
+            oku = uygula.ExecuteReader(CommandBehavior.CloseConnection);
+            while (oku.Read())
+            {
+                if (txtSehir.Items.Contains(oku.GetString(1).ToString()) != true)
+                {
+                    veri.Add(oku.GetString(1));
+                    txtSehir.Items.Add(oku.GetString(1));
+                }
+            }
+            conn.Close();
+            oku.Close();
+            txtSehir.AutoCompleteCustomSource = veri;
+        }
+
+        private void btnKayit_Click(object sender, EventArgs e)
+        {
+            YeniKaydet();
+            Listele();
+        }
+
+        private void YeniKaydet()
+        {
+            uygula = new SqlCommand();
+            uygula.CommandType = CommandType.StoredProcedure;
+            uygula.CommandText ="[S_Doktor]";
+            uygula.Connection = conn;
+            uygula.Connection.Open();
+
+            uygula.Parameters.Add("@unvan", SqlDbType.NVarChar, 50).Value = txtUnvan.Text;
+            uygula.Parameters.Add("@doktorAdi", SqlDbType.NVarChar, 50).Value = txtAd.Text;
+            uygula.Parameters.Add("@sehirId", SqlDbType.Int).Value = txtSehirId.Text;
+
+            uygula.ExecuteNonQuery();
+            uygula.Connection.Close();
+            conn.Close();
+            MessageBox.Show("Kayıt Başarılı..");
+        }
+
+        private void txtSehir_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string query = $"Select il_no from iller where isim='{txtSehir.Text}'";
+            uygula = new SqlCommand(query);
+            uygula.Connection = conn;
+            uygula.Connection.Open();
+            txtSehirId.Text = uygula.ExecuteScalar().ToString();
+            uygula.Connection.Close();
+
+            txtIlceler.Items.Clear();
+            txtIlceler.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            AutoCompleteStringCollection veri = new AutoCompleteStringCollection();
+            string query1 = $"Select * from ilceler where il_no='{txtSehirId.Text}'";
+            uygula = new SqlCommand(query1, conn);
+            uygula.Connection.Open();
+            oku = uygula.ExecuteReader(CommandBehavior.CloseConnection);
+            while (oku.Read())
+            {
+                if (txtIlceler.Items.Contains(oku.GetString(1).ToString()) != true)
+                {
+                    veri.Add(oku.GetString(1));
+                    txtIlceler.Items.Add(oku.GetString(1));
+                }
+            }
+            conn.Close();
+            oku.Close();
+            txtIlceler.AutoCompleteCustomSource = veri;
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            DialogResult mesaj;
+            mesaj = MessageBox.Show("Bu kaydı silmek üzeresiniz", "Silme işlemi", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (mesaj == DialogResult.Yes)
+            {
+                Sil();
+            }
+            
+        }
+
+        private void Sil()
+        {
+            if (secimId > 0)
+            {
+                uygula = new SqlCommand();
+                uygula.CommandType = CommandType.StoredProcedure;
+                uygula.CommandText = "[d_Doktor]";
+                uygula.Connection = conn;
+                uygula.Connection.Open();
+
+                uygula.Parameters.Add("@Id", SqlDbType.Int).Value = secimId;
+                uygula.ExecuteNonQuery();
+                uygula.Connection.Close();
+                conn.Close();
+                MessageBox.Show("Kayıt Silinmiştir.");
+                Temizle();
+                Listele();
+            }
+        }
+
+        private void Temizle()
+        {
+            txtAd.Text = "";
+            txtSehir.Text = "";
+            txtSehirId.Text = "";
+            txtUnvan.Text = "";
+            secimId = -1;
+            
+        }
+
+        public int secimId = -1;
+        private void Liste_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                secimId = int.Parse(Liste.CurrentRow.Cells[0].Value.ToString());
+            }
+            catch (Exception)
+            {
+                secimId = -1;
+            }
+            if (secimId > 0)
+            {
+                Ac();
+            }
+        }
+
+        private void Ac()
+        {
+            txtAd.Text = Liste.CurrentRow.Cells[2].Value.ToString();
+            txtSehirId.Text = Liste.CurrentRow.Cells[3].Value.ToString();
+            txtUnvan.Text = Liste.CurrentRow.Cells[1].Value.ToString();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            Guncelle();
+            Listele();
+        }
+
+        private void Guncelle()
+        {
+            uygula = new SqlCommand();
+            uygula.CommandType = CommandType.StoredProcedure;
+            uygula.CommandText = "[U_Doktor]";
+            uygula.Connection = conn;
+            uygula.Connection.Open();
+
+            uygula.Parameters.Add("@Id", SqlDbType.Int).Value = secimId;
+            uygula.Parameters.Add("@unvan", SqlDbType.NVarChar, 50).Value = txtUnvan.Text;
+            uygula.Parameters.Add("@doktorAdi", SqlDbType.NVarChar, 50).Value = txtAd.Text;
+            uygula.Parameters.Add("@sehirId", SqlDbType.Int).Value = txtSehirId.Text;
+
+            uygula.ExecuteNonQuery();
+            uygula.Connection.Close();
+            conn.Close();
+            MessageBox.Show("Güncelleme Başarılı..");
+        }
+
+
+    }
+}
